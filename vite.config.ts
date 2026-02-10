64ae9085bf9786034bf188f8568ff98ba1d237fd
天堂守护者 <dwm97317@163.com>
2026-02-10 19:03:33 +0800

    fix(build): add legacy plugin for android compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+import legacy from '@vitejs/plugin-legacy';
+
 // https://vitejs.dev/config/
 export default defineConfig({
     base: './',
-    plugins: [react()],
+    plugins: [
+        react(),
+        legacy({
+            targets: ['chrome >= 60', 'android >= 9'],
+            additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+        })
+    ],
+    build: {
+        target: 'es2015'
+    }
 })
